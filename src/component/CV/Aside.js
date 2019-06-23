9d14c6c3a5e8fd346f1b4c0d369142b179bb440e
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-23 16:44:19 -0400

    WIP on confidence bars on CV

--- src/component/CV/Aside.js
+++ src/component/CV/Aside.js
@@ -6,6 +6,17 @@ import { FaGithubSquare } from 'react-icons/fa';
 export default props => {
   // const { email, spoken, notions, address } = props.main;
   // const { url, className } = props.social;
+
+  var skills = this.props.data.skills.map(function(skills) {
+    var className = 'bar-expand ' + skills.name.toLowerCase();
+    return (
+      <li key={skills.name}>
+        <span style={{ width: skills.confidence }} className={className} />
+        <em>{skills.name}</em>
+      </li>
+    );
+  });
+
   return (
     <div className='aside'>
       <div className='aside-section'>
@@ -29,6 +40,19 @@ export default props => {
         <p>notions</p>
       </div>
       <div className='aside-section'>
+        <div className='row skill'>
+          <div className='three columns header-col'>
+            <h2>
+              <span>Programming</span>
+            </h2>
+          </div>
+
+          <div className='nine columns main-col'>
+            <div className='bars'>
+              <ul className='skills'>{skills}</ul>
+            </div>
+          </div>
+        </div>
         {/* <h2>Programming</h2>
         <p>JavaScript</p>
         <p>(ES6, Node.js)</p>
