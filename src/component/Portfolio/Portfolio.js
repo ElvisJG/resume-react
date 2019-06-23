0fa2476f52e439f4779050d9cfeaa5ce75c46010
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-22 21:08:56 -0400

    CSS bugs and minor bugs in skeleton, here comes the fun part - contact form, refactoring code, and adding responsiveness

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Portfolio.css';
+import wanderlust from './wanderlust.jpg';
 
 export default () => {
   return (
     <div className='portfolio-wrapper'>
-      <div className='portfolio-hero-text'>
-        <h1>Portfolio</h1>
+      <div className='profile-card'>
+        <img
+          className='portfolio-img'
+          src={wanderlust}
+          alt='Underwater Image'
+        />
+        <div className='portfolio-hero'>
+          <div className='portfolio-hero-text'>
+            <h1>Wanderlust</h1>
+            <h3>
+              Wanderlust's Marketing Page, developed using HTML, CSS, SCSS, and
+              JS
+            </h3>
+            <a href='https://marketing-wanderlust.netlify.com/'>
+              <button>Visit Wanderlust</button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   );
