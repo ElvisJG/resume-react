61c0557faf29778a4018dd697f162ec8475ce575
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-06-24 15:50:56 -0400

    lazy load, optimization, redirects

--- src/App.js
+++ src/App.js
@@ -1,40 +1,42 @@
-import React, { Component } from 'react';
+import React, { PureComponent, Suspense, lazy } from 'react';
 import { Route } from 'react-router-dom';
-
-import Nav from './component/Navigation/Navbar';
-import Home from './component/Home/Homepage';
-import CV from './component/CV/CV';
-import Portfolio from './component/Portfolio/Portfolio';
-// import Contact from './component/Contact/ContactForm';
 
 import resumeData from './resumeData.json';
 
-export default class App extends Component {
+const Nav = lazy(() => import('./component/Navigation/Navbar'));
+const Home = lazy(() => import('./component/Home/Homepage'));
+const CV = lazy(() => import('./component/CV/CV'));
+const Portfolio = lazy(() => import('./component/Portfolio/Portfolio'));
+// const Contact = lazy(() => import("./component/Contact/ContactForm"));
+
+export default class App extends PureComponent {
   constructor() {
     super();
     this.state = {
-      resumeData: resumeData
+      resumeData
     };
   }
 
   render() {
     const { portfolio, main, resume } = this.state.resumeData;
     return (
-      <div className='app'>
-        <Nav />
-        <Route path='/' exact render={props => <Home main={main} />} />
-        <Route
-          path='/cv'
-          exact
-          render={props => <CV resume={resume} main={main} />}
-        />
-        <Route
-          path='/portfolio'
-          exact
-          render={props => <Portfolio portfolio={portfolio} />}
-        />
-        {/* <Route path='/contact' exact render={props => <Contact />} /> */}
-      </div>
+      <Suspense fallback={<h1>loading..</h1>}>
+        <div className='app'>
+          <Nav />
+          <Route path='/' exact render={props => <Home main={main} />} />
+          <Route
+            path='/cv'
+            exact
+            render={props => <CV resume={resume} main={main} />}
+          />
+          <Route
+            path='/portfolio'
+            exact
+            render={props => <Portfolio portfolio={portfolio} />}
+          />
+          {/* <Route path='/contact' exact render={props => <Contact />} /> */}
+        </div>
+      </Suspense>
     );
   }
 }
