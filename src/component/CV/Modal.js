2b27f8c6f61627c3310e644b277f1b9296c9705f
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-08-27 23:06:04 -0400

    added escape key hotkey

--- src/component/CV/Modal.js
+++ src/component/CV/Modal.js
@@ -1,19 +1,26 @@
-import React, { useRef, useEffect } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
 import Contact from './Contact';
 
 const Modal = ({ isShowing, hide }) => {
-  const ref = useRef();
-  useKeyUp('Escape', hide);
-  useOnClickOutside(ref, hide);
+  const keyListenersMap = new Map([[27, hide]]);
 
+  useEffect(() => {
+    function keyListener(e) {
+      const listener = keyListenersMap.get(e.keyCode);
+      return listener && listener(e);
+    }
+    document.addEventListener('keydown', keyListener);
+
+    return () => document.removeEventListener('keydown', keyListener);
+  });
   return (
     <>
       {isShowing
-        ? ReactDOM.createPortal(
+        ? createPortal(
             <div className='modal-overlay'>
-              <Contact hide={hide} ref={ref} />
+              <Contact hide={hide} />
             </div>,
             document.body
           )
@@ -21,36 +28,5 @@ const Modal = ({ isShowing, hide }) => {
     </>
   );
 };
-
-function useKeyUp(targetKey, handler) {
-  const onKeyUp = ({ key }) => {
-    if (key === targetKey) handler();
-  };
-
-  useEffect(() => {
-    window.addEventListener('keyup', onKeyUp);
-
-    return () => {
-      window.removeEventListener('keyup', onKeyUp);
-    };
-  }, []);
-}
-
-function useOnClickOutside(ref, handler) {
-  useEffect(() => {
-    const listener = event => {
-      if (!ref.current || ref.current.contains(event.target)) return;
-      handler(event);
-    };
-
-    document.addEventListener('mousedown', listener);
-    document.addEventListener('touchstart', listener);
-
-    return () => {
-      document.removeEventListener('mousedown', listener);
-      document.removeEventListener('touchstart', listener);
-    };
-  }, [ref, handler]);
-}
 
 export default Modal;
