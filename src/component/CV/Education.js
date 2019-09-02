5fa3f77b378f37be7915955eac4e9496abdbf153
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-09-02 09:44:40 -0400

    improved UI for education, refactoring

--- src/component/CV/Education.js
+++ src/component/CV/Education.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaGraduationCap } from 'react-icons/fa';
 
 export default props => {
   return (
@@ -9,15 +10,20 @@ export default props => {
 
       {props.resume.education.map(edu => (
         <div className='cv-edu' key={edu.school}>
-          <h5>{edu.graduated}</h5>
+          <div className='cv-school'>
+            <h4>{edu.school}</h4>
+            <span className='cap'>
+              {edu.complete === 'true' ? <FaGraduationCap size='2rem' /> : null}
+            </span>
+            <em>
+              <h5>{edu.graduated}</h5>
+            </em>
+          </div>
           <div className='cv-edu-mid'>
             <em>
               <p>{edu.degree}</p>
             </em>
             <p>{edu.description}</p>
-          </div>
-          <div className='cv-school'>
-            <p>{edu.school}</p>
           </div>
         </div>
       ))}
